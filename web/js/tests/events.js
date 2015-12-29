9502dd4f52091c63891a845c1e0e09fc0a2886cb
Maria <mkdirwtf@gmail.com>
2015-12-29 04:07:37 +0300

    Delete events.js

--- web/js/tests/events.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener( 'keydown', function(event) {
-    switch( event.keyCode ) {
-      case  38 : pressed_up(); break;
-      case  37 : pressed_left(); break;
-      case  39 : pressed_right(); break;
-      case  40 : pressed_down(); break;
-    }
-});
